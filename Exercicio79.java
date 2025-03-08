7d35b796b4eff6d0bbc6891870e4afa47722bd60
Gabriel Muchon Pavanelli <gabriel.mp13@hotmail.com>
2025-03-08 17:36:21 -0300

    Adicionado Exercicio80.java Atualizado Exercicio79.java

--- Exercicio79.java
+++ Exercicio79.java
@@ -1,6 +1,6 @@
 
 
-/* Exerc
+/* Exercício 79 - Desenvolvido por Gabriel Muchon Pavanelli. Faculdade Impacta de Tecnologia
 79. Crie um programa que preencha automaticamente (usando lógica, não apenas atribuindo diretamente) um vetor numérico com 15 posições com os primeiros elementos da sequência de Fibonacci:
 1 1 2 3 5 8 13  21  34  55  89  144  233  377  610  987 ( valores da sequência de Fibonacci)
 0 1 2 3 4 5 6 7 8 9 10  11 12 13 14 15 (posição do índice)
---
+++ Exercicio80.java
@@ -0,0 +1,36 @@
+/*
+Exercício 80 - Desenvolvido por Gabriel Muchon Pavanelli. Faculdade Impacta de Tecnologia
+
+80. Crie um programa que preencha automaticamente um vetor numérico com 7 números
+gerados aleatoriamente pelo computador e depois mostre os valores gerados na tela.
+ */
+import java.util.Random;
+
+public class Exercicio80 {
+    public static void main(String[] args) {
+        int[] vetor = new int[7]; // Vetor com 7 posições
+        Random gerador = new Random(); // Objeto da classe random
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = gerador.nextInt(100); // Gera um número entre 0 e 99
+        }
+        /**
+         * i:  é uma variável contadora
+         * int i = 0:  inicializa a variável i com o valor 0.
+         * i < vetor.length: O laço continua enquanto i for menor que o tamanho do vetor (7).
+         * ---> length: comprimento" ou "tamanho"
+         * i++: A cada iteração, a variável i é incrementada em 1.
+         *
+         * Esse laço percorre todos os índices do vetor (de 0 a 6) e executa o código dentro dele
+         */
+
+        // Mostra os valores gerados
+        System.out.println("Valores gerados: ");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+            /**
+             *
+             */
+        }
+    }
+}
