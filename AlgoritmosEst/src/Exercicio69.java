12e99b551cf3068015669feebc9d29bb9474e3b7
Gabriel Muchon Pavanelli <gabriel.mp13@hotmail.com>
2025-03-09 15:11:35 -0300

    Melhorado a organização do repositório

--- AlgoritmosEst/src/Exercicio69.java
+++
@@ -1,9 +0,0 @@
-public class Exercicio69 {
-    public static void main(String[] args) {
-        // Estrutura "para" para a contagem
-        for (int i = 0; i <= 40; i += 5) {
-            System.out.print(i + " ");
-        }
-        System.out.println("Acabou!");
-    }
-}
