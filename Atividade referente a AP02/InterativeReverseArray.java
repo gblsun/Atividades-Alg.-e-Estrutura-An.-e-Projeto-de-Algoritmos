aaebd1454bb651b2d6fb2dab3d9b7bb87a86142c
Gabriel Mascavo <gabrimac11@gmail.com>
2025-05-12 20:50:40 -0300

    Update and rename interativereversearray.java to InterativeReverseArray.java

--- Atividade referente a AP02/InterativeReverseArray.java
+++ Atividade referente a AP02/InterativeReverseArray.java
@@ -1,24 +1,19 @@
-public class interativereversearray {
+public class InterativeReverseArray {
 
-    // Reverte o array
     public static void reverseArray(int[] array) {
-        int inicio = 0;                // Índice do início do array
-        int fim = array.length - 1;   // Índice do fim do array
-
-        // Enquanto o índice inicial for menor que o final
+        int inicio = 0;
+        int fim = array.length - 1;
+        
         while (inicio < fim) {
-            // Troca os elementos nas posições start e end
             int temp = array[inicio];
             array[inicio] = array[fim];
             array[fim] = temp;
 
-            // Move os índices em direção ao centro
             inicio++;
             fim--;
         }
     }
 
-    // Método para imprimir o array
     public static void printArray(int[] array) {
         for (int value : array) {
             System.out.print(value + " ");
@@ -26,7 +21,6 @@ public class interativereversearray {
         System.out.println();
     }
 
-    // Método principal
     public static void main(String[] args) {
         int[] meuArray = {1, 2, 3, 4, 5};
 
